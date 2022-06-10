e548287688a8943277c49571977c7c0d4e9b5b05
charliezong <charliezong18@gmail.com>
2022-06-09 21:20:04 -0400

    initial commit!

---
+++ src/main/java/com/example/kaimall/KaimallApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaimallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaimallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kaimall/KaimallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kaimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaimallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
